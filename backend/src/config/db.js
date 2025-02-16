a11b881bf5c9f2533565013de65d9a40aa037d7d
cosmotropia <fsapiains.c@gmail.com>
2025-02-15 21:04:28 -0500

    updated db config for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
 require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
-const pool = new Pool({
-  host: 'localhost',
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  allowExitOnIdle: true
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: 'localhost',
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        allowExitOnIdle: true
+      }
+)
 
 pool.on('connect', () => {
   console.log('Conectado a la base de datos PostgreSQL:', process.env.DATABASE_NAME);
