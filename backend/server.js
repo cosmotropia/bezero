aba2a642a0478cad3e05b273b3e33e26f4f44df4
cosmotropia <fsapiains.c@gmail.com>
2025-02-17 22:03:17 -0500

    test uploads imgs folder

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,39 @@
+const express = require("express")
+const fs = require("fs")
+const path = require("path")
 const app = require("./app")
 const pool = require("./src/config/db")
-require("dotenv").config({ path: require("path").resolve(__dirname, "./.env") });
+require("dotenv").config({ path: require("path").resolve(__dirname, "./.env") })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+const uploadDir = path.join(__dirname, "public/uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  console.log("La carpeta public/uploads no existe. Creándola...");
+  fs.mkdirSync(uploadDir, { recursive: true });
+} else {
+  console.log("La carpeta public/uploads ya existe");
+}
+
 const testDatabaseConnection = async () => {
   try {
-    const res = await pool.query("SELECT NOW()"); // Consulta de prueba
-    console.log("✅ Conectado a PostgreSQL en:", res.rows[0].now);
+    const res = await pool.query("SELECT NOW()")
+    console.log("Conectado a PostgreSQL en:", res.rows[0].now)
   } catch (error) {
-    console.error("❌ Error conectando a PostgreSQL:", error);
+    console.error("Error conectando a PostgreSQL:", error)
     process.exit(1);
   }
-};
+}
 
 const startServer = async () => {
-  await testDatabaseConnection()
+  await testDatabaseConnection();
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
   });
-};
+}
 
 startServer()
+
 
